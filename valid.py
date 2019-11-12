f2f2c96a892aef642976e1f37dfaf490d66f7106
faridrah0514 <fr.farid.rahman@gmail.com>
2019-11-12 12:28:45 +0700

    remove unnecessary comment and variables

--- valid.py
+++ valid.py
@@ -1,27 +1,18 @@
-import csv
 import json
 
 ccValid=[]
 ccInvalid=[]
-#ketentuan
-digit_awal=['4', '5', '6']
-panjang=16
 angka=['0','1','2','3','4','5','6','7','8','9']
-
 
 file = open('ccNasabah.json', 'r')
 output=json.load(file)
 for i in output:
     valid=True
-    no_credit_card=i["noCreditCard"]
-    # print(no_credit_card)
-    # print(no_credit_card)
-    if no_credit_card[0] not in digit_awal or len(i["noCreditCard"].replace("-","")) != panjang:
+    if i["noCreditCard"][0] not in ['4','5','6'] or len(i["noCreditCard"].replace("-","")) != 16:
         valid=False
-    jml=''
+
     for number in i["noCreditCard"].replace("-",""):
         jml=number*4
-        # print(i["noCreditCard"].replace("-","").count(jml))
         if number not in angka:
             valid=False
             break
@@ -30,14 +21,11 @@ for i in output:
             break
     
     if "-" in i["noCreditCard"]:
-        no_credit_card1=i["noCreditCard"].split("-")
-        # print(no_credit_card1)
         for k in i["noCreditCard"].split("-"):
             if len(k) > 4:
                 valid=False
                 break
 
-    
     if valid == True:
         ccValid.append(i)
     else:
